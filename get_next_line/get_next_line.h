376d32a05e1a4f369630674d3d7b11dd4251d67e
Isabel Fernandez abad <isfernan@c1r14s3.42madrid.com>
2022-11-02 19:12:37 +0100

    Readme

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: isfernan <isfernan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/21 12:52:45 by isfernan          #+#    #+#             */
-/*   Updated: 2020/07/20 15:55:34 by isfernan         ###   ########.fr       */
+/*   Updated: 2022/11/02 19:08:08 by isfernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include "/Users/isfernan/42cursus/cub3d/cub3d_fake/libft/libft.h"
+#include "../libft/libft.h"
 //# include "/Users/isfernan/42cursus/miniRT/miniRT_fake/libft/libft.h"
 
 void	ft_gnl(char *buff[], char **line, int *nb, int fd);
